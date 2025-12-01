5419b619679705daceebc991c4600e9a9dc61f74
Beautero-ks <beauterokenne37@gmail.com>
2025-12-01 21:18:09 +0100

    fix the setTimeOut when sending the admin mail

--- app/api/utils/mailer.ts
+++ app/api/utils/mailer.ts
@@ -8,7 +8,9 @@ export async function sendAdminMail(order: any) {
   }
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.ADMIN_EMAIL,
       pass: process.env.ADMIN_EMAIL_PASSWORD
